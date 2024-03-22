83bf1148dba88d899c5238e0faab5d5aec7c7c4b
aniket_charjan <aniketcharjan3@gmail.com>
2024-03-22 13:47:03 +0530

    removed code

--- src/settings/config.py
+++ src/settings/config.py
@@ -84,41 +84,6 @@ profile_id = getENVValue(amc_configs["AMAZON_DSP_SETTINGS"]["PROFILE_ID"])
 advertiser_id = getENVValue(amc_configs["AMAZON_DSP_SETTINGS"]["ADVERTISER_ID"])
 
 
-# print("\nPrinting env variables: \n")
-# print("storage_account_name:", storage_account_name)
-# print("account_access_key:", account_access_key)
-# print("container_name:", container_name)
-# print("sas_token:", sas_token)
-# print("connection_string:", connection_string)
-# print("db_server:", db_server)
-# print("db_database:", db_database)
-# print("db_username:", db_username)
-# print("db_password:", db_password)
-# print("db_driver:", db_driver)
-# print("refresh_token:", refresh_token)
-# print("client_id:", client_id)
-# print("client_secret:", client_secret)
-# print("profile_id:", profile_id)
-# print("advertiser_id:", advertiser_id)
-# print("reckitt_okta_api_token:", reckitt_okta_api_token)
-
-
-# refresh_token = 'Atzr|IwEBIA_xKM5nxz7_fTLj97VcZC_Rys7atwKdnbHMkeWWSg2r1qyTcG9ni2x0PUX-JVtNuDoENIwWPwduXtQvxTt7fJr0x65aqScd3JjLA_2jkiHccMRAHESQ9dIa0SYmMOwrhV7CazdF17eALlKJM8CxJUSFPMpUpJdNmudutg5oKl95u6imvqS76TLHotMjeFvNNU8uTUmH7j2C69xpnyZgdp3tjwG6ef18sdR5IVVzlO3ohoIUtH1nMvHb61GYAQHLp9zB1ugsMZhhz1SHeuH_cpGPaM4_oA0Meq5x1Pcg_YrYpC74kGsBk74Ze2mvanGVMMYTleF0jccOpfuDLzdYXaYD-wJGB16GNGisM0Fa4bsOiZXI9z-V-GbI9_FOerdKBHi8DI52E8VXe-Ilf8DrNwhLAFSlCBLXDv-HN7_XZWATnx5j0hfPAVIkHeBTZFLc-sGzuQKhj4H3cZR-6hBpFQ39'
-# client_id = 'amzn1.application-oa2-client.7dd43a9855b74bf69ffa42199fa81399'
-# client_secret = '11dccfe1169dba8d7e21aeeb7248654b9f0c7558361dbd761a6553274ad8ac47'
-# profile_id="475484054892114"
-# advertiser_id="5797658410201"
-
-# shared_client_secret = getENVValue(amc_configs["PLACEORDER_API_AUTH_SETTINGS"]["SHARED_CLIENT_SECRET"])
-
-# tenant_id = getENVValue(amc_configs["CREATECAMPAIGN_API_SETTINGS"]["TENET_ID"])
-# cc_api_client_id = getENVValue(amc_configs["CREATECAMPAIGN_API_SETTINGS"]["CC_API_CLIENT_ID"])
-# cc_api_client_secret = getENVValue(amc_configs["CREATECAMPAIGN_API_SETTINGS"]["CC_API_CLIENT_SECRET"])
-# subscription_id = getENVValue(amc_configs["CREATECAMPAIGN_API_SETTINGS"]["SUBSCRIPTION_ID"])
-# resource_group = getENVValue(amc_configs["CREATECAMPAIGN_API_SETTINGS"]["RESOURCE_GROUP"])
-# data_factory_name = getENVValue(amc_configs["CREATECAMPAIGN_API_SETTINGS"]["DATA_FACTORY_NAME"])
-# pipeline_name = getENVValue(amc_configs["CREATECAMPAIGN_API_SETTINGS"]["PIPELINE_NAME"])s
-
 logger.info("Configs Loaded")
 
 
